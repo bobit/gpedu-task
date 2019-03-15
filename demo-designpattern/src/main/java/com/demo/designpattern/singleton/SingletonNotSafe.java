b7f249d1bf6ab8170b60fc709bd745b9341c1014
zhangb <iloveapi@163.com>
2019-03-15 16:09:48 +0800

    update

--- demo-designpattern/src/main/java/com/demo/designpattern/singleton/SingletonNotSafe.java
+++ demo-designpattern/src/main/java/com/demo/designpattern/singleton/SingletonNotSafe.java
@@ -3,18 +3,21 @@ package com.demo.designpattern.singleton;
 /**
  * 单例模式（Singleton Pattern）
  * 线程不安全实例
+ *
  * @Author: zhangb
- * @Date: 2018/12/13 23:02
+ * @Date: 2019/3/9 16:55
  */
 public class SingletonNotSafe {
 
     private static SingletonNotSafe singleton = null;
+
     //限制产生多个对象
-    private SingletonNotSafe(){
+    private SingletonNotSafe() {
     }
+
     //通过该方法获得实例对象
-    public static SingletonNotSafe getSingleton(){
-        if(singleton == null){
+    public static SingletonNotSafe getSingleton() {
+        if (singleton == null) {
             singleton = new SingletonNotSafe();
         }
         return singleton;
